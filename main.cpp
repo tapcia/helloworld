c2efc54326510a17e0531828024facb2b6fc4d8d
tapcia <tapcia@gmail.com>
2016-02-02 16:53:11 +0100

    helloworld

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, 
+system("pause") or input loop */
+
+// komentarz niepodzielny
+using namespace std;
+
+int main() {
+	
+	cout << "Hello World" << endl;
+	return 0;
+}
